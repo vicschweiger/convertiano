6413d906db068bc20d5778b8f870fd1edabc9cd9
vicschweiger <134645175+vicschweiger@users.noreply.github.com>
2024-06-28 22:13:18 -0300

    [200~Adicionadino header e footer mais respectivas estilizações.

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    pass
+    return render_template('base.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
