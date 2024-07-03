4bf2fdfcd74aad7dfefc9e267207fc06d6749d04
vicschweiger <134645175+vicschweiger@users.noreply.github.com>
2024-07-03 14:12:44 -0300

    Adicionada estilização da página principal e ícone das páginas da aplicação.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template('base.html')
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
